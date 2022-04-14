9da1c169f8eca7b092be88e3e5cc97860c24ca53
pierreyoda <pierreyoda@users.noreply.github.com>
2022-04-14 20:12:47 +0200

    :arrow_up: Upgrade dependencies

--- rustifterm/src/main.rs
+++ rustifterm/src/main.rs
@@ -1,28 +1,31 @@
 mod client;
 mod errors;
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-use clap::{App, Arg};
+use clap::Parser;
 
 use client::IFTerminalClient;
 use errors::IFtResult;
 use rustifzm;
 
+#[derive(Debug, Parser)]
+#[clap(
+    author = "pierreyoda <pierreyoda@users.noreply.github.com>",
+    version = "0.0.1",
+    about = "This terminal client for the rustifzm Z-machine interpreter allows to play classic Interactive Fiction games like Zork."
+)]
+struct Args {
+    #[clap(parse(from_os_str), help = "The input story file to help.")]
+    story_file: PathBuf,
+}
+
 fn main() -> IFtResult<()> {
-    let matches = App::new("rustifterm")
-        .version("0.0.1")
-        .author("pierreyoda <pierreyoda@users.noreply.github.com>")
-        .about("This terminal client for the rustifzm Z-machine interpreter allows to play classic Interactive Fiction games like Zork.")
-        .arg(Arg::with_name("STORY")
-            .help("The input story file to play.")
-            .required(true)
-            .index(1))
-        .get_matches();
+    let args = Args::parse();
 
-    let story_file_name = matches.value_of("STORY").unwrap();
-    let story_file_path = Path::new(story_file_name);
+    let story_file_name = args.story_file;
+    let story_file_path = Path::new(&story_file_name);
 
-    let mut client = IFTerminalClient::with_story_file(&story_file_path)?;
+    let mut client = IFTerminalClient::with_story_file(story_file_path)?;
     client.run()
 }
